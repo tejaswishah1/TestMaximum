39be78895633b6b4287a4c2dccb53b74dae92d0f
tejaswishah1 <tejaswishah1998@gmail.com>
2020-10-20 15:25:52 +0530

    [tejas] add initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestMaximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Maximum of the three numbers");
+        }
+    }
+    
+}
