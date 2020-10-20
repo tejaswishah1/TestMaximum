d1a3fa2af78c110d948c2d7dc29d3fc3edb7b3da
tejaswishah1 <tejaswishah1998@gmail.com>
2020-10-20 18:01:18 +0530

    [tejas] refactor Extended maximum value for more than 3 parameters

--- Program.cs
+++ Program.cs
@@ -7,40 +7,19 @@ namespace TestMaximum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter first number: ");
-            int a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter second number: ");
-            int b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter third number: ");
-            int c = Convert.ToInt32(Console.ReadLine());
 
-            int[] intArray = { a,b,c };
+
+            int[] intArray = { 112, 344, 432, 555, 678 };
             Maximum<int> generic = new Maximum<int>(intArray);
             generic.PrintMaxValue();
-
-            Console.WriteLine("Enter First decimal value: ");
-            double x = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter second decimal value: ");
-            double y = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter third decimal value: ");
-            double z = Convert.ToDouble(Console.ReadLine());
-
-            double[] doubleArray = { x, y, z };
+            double[] doubleArray = { 11.2, 34.4, 4.32, 55.5, 6.78 };
             Maximum<double> genericDouble = new Maximum<double>(doubleArray);
             genericDouble.PrintMaxValue();
-
-            Console.WriteLine("Enter first value: ");
-            string value1 = Console.ReadLine();
-            Console.WriteLine("Enter second value: ");
-            string value2 = Console.ReadLine();
-            Console.WriteLine("Enter third value: ");
-            string value3 = Console.ReadLine();
-
-            string[] stringArray = {value1,value2,value3 };
+            string[] stringArray = { "111", "222", "333", "55", "999" };
             Maximum<string> genericString = new Maximum<string>(stringArray);
             genericString.PrintMaxValue();
 
         }
     }
-    
+
 }
